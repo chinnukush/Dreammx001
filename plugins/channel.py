42988678966e20202e8327af997a9c96dc224bd2
chinnukush <chinnukushal123@gmail.com>
2025-12-23 20:28:18 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -369,10 +369,9 @@ async def send_movie_update(bot, base_name):
 
             text = generate_movie_message(movie_doc, base_name)
             buttons = InlineKeyboardMarkup([[
-                InlineKeyboardButton(
-                    'ɢᴇᴛ ғɪʟᴇs',
-                    url=f"https://t.me/{temp.U_NAME}?start=getfile-{base_name.replace(' ', '-')}"
-                )
+                InlineKeyboardButton('♻️ ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ɢᴇᴛ ғɪʟᴇ ♻️', url=f"https://t.me/{temp.U_NAME}?start=getfile-{base_name.replace(' ', '-')}")
+            ],[
+                InlineKeyboardButton('🔍 Ⓜ︎Ⓞ︎Ⓥ︎Ⓘ︎Ⓔ︎ Ⓖ︎Ⓡ︎Ⓞ︎Ⓤ︎Ⓟ︎ 🔎', url=f'https://t.me/+q101Ee1l-ycwZTU1')    
             ]])
             size=(2560, 1440) if LANDSCAPE_POSTER and TMDB_POSTER and movie_doc.get("is_backdrop") and not movie_doc.get("error_tmdb") else (853, 1280)
             if movie_doc.get("poster_url") and not LINK_PREVIEW:
@@ -418,11 +417,10 @@ async def update_movie_message(bot, base_name):
 
         text = generate_movie_message(movie_doc, base_name)
         buttons = InlineKeyboardMarkup([[
-            InlineKeyboardButton(
-                'ɢᴇᴛ ғɪʟᴇs',
-                url=f"https://t.me/{temp.U_NAME}?start=getfile-{base_name.replace(' ', '-')}"
-            )
-        ]])
+                InlineKeyboardButton('♻️ ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ɢᴇᴛ ғɪʟᴇ ♻️', url=f"https://t.me/{temp.U_NAME}?start=getfile-{base_name.replace(' ', '-')}")
+            ],[
+                InlineKeyboardButton('🔍 Ⓜ︎Ⓞ︎Ⓥ︎Ⓘ︎Ⓔ︎ Ⓖ︎Ⓡ︎Ⓞ︎Ⓤ︎Ⓟ︎ 🔎', url=f'https://t.me/+q101Ee1l-ycwZTU1')    
+            ]])
 
         message_id = movie_doc.get("message_id")
         is_photo = movie_doc.get("is_photo", False)
