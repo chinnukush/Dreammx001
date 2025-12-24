3e2d4043c16067748d338a4f94656220d1d12a97
chinnukush <chinnukushal123@gmail.com>
2025-12-24 21:52:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -121,7 +121,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # Channel & Group Links Configuration
 # ============================
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+nOdVKAzF-mQyYTU1') # Group link for the bot
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/hari_moviez') # Owner link for the bot
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/harikushal') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/+jgLqztoklls5MjRl') # Update channel link for the bot
 
 # ============================
